742c63c9aee1a6e818fa05b3c74a75b3d422f29c
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2023-01-07 22:16:58 +0600

    Update TodoItem.tsx

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -22,18 +22,13 @@ export const TodoItem: React.FC<Props> = ({
 }) => {
 	return (
 		<>
-			<div className={scss.todoItems}>
-				<div
-					className={scss.todo__item}
-					style={todo.complete ? ready : notReady}
-				>
-					{todo.text}
-					<button onClick={() => completeTodo(id)}>
-						{todo.complete ? "Отменить" : "Выполнить"}
-						complete
-					</button>
-					<button onClick={() => deleteTodo(id)}>delete</button>
-				</div>
+			<div className={scss.todo__item} style={todo.complete ? ready : notReady}>
+				{todo.text}
+				<button onClick={() => completeTodo(id)}>
+					{todo.complete ? "Отменить" : "Выполнить"}
+					complete
+				</button>
+				<button onClick={() => deleteTodo(id)}>delete</button>
 			</div>
 		</>
 	);
