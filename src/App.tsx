bb04716f90a718b6b4a70010338432faf76d0589
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2023-01-28 13:23:23 +0600

    Decorated the design 🎉

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.scss";
 import { Wrapper } from "./components/wrapper/Wrapper";
 
 export const App = () => {
 	return (
 		<>
+			<div>
+				<a href="https://vitejs.dev" target="_blank">
+					<img src="/vite.svg" className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://reactjs.org" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Vite + React + TS</h1>
 			<Wrapper />
 		</>
 	);
